f6f936b1fcc7b4e8f5f02d300d6d22c7524a6ca2
Haleem Yousef <97648449+haleemyousef@users.noreply.github.com>
2022-12-20 08:56:10 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import math
+import numpy
+import tqdm
+
+# [102$$$ ohms, sps, [r1, r2, r3, r4]]
+
+class main:
+  def __init__(self):
+    self.available_resistors = []
+    self.resultant_configs = []
+    
+  def exact_match(self):
+    if input is in self.available_resistors:
+      return True
+  
+  def calc_comb(self):
+    
+    
+  def special_cases(self):
+    
