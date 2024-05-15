36358b7d04e1747c647f2ceaaf30e82933fdc006
haleemyousef <abdulhalem.abusalem1@gmail.com>
2024-05-15 19:47:46 +0100

    implementation done, now remerging into main.

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 resistor_inventory = [1000, 2000]
 calculated_combinations = []
 
-x = len(resistor_inventory)
-k1 = x
-k2 = x^2 + x
-k3 = 2*x^3 + 2*x^2
-# k4_1 = 4*x^4 + 4*x^3
-# k4_2 = x^4 + 2*x^3 + 2*x^2 + x
+n = len(resistor_inventory)
+k1 = n
+k2 = n^2 + n
+k3 = 2*n^3 + 2*n^2
+# k4_1 = 4*n^4 + 4*n^3
+# k4_2 = n^4 + 2*n^3 + 2*n^2 + n
 
 calculated_combinations += resistor_inventory
-x -= 1 # decrease by 1 to be used as the array index
+x = 0 # array index
 
-while x >= 0:
+while x < n:
     y = x
-    while y >= 0:
+    while y < n:
         series = resistor_inventory[x] + resistor_inventory[y]
         calculated_combinations.append(series)
         parallel = (resistor_inventory[x] * resistor_inventory[y]) / series
         calculated_combinations.append(parallel)
-        y -= 1
-    x -= 1
+        y += 1
+    x += 1
+
+
 
 print(calculated_combinations)
