0fc14406de60a387b329eee92464ffcc7cd33a73
haleemyousef <abdulhalem.abusalem1@gmail.com>
2024-05-15 19:23:04 +0100

    working python implementation of up to k = 2

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-import math
-import numpy
-import tqdm
+resistor_inventory = [1000, 2000]
+calculated_combinations = []
 
-# [102$$$ ohms, sps, [r1, r2, r3, r4]]
+x = len(resistor_inventory)
+k1 = x
+k2 = x^2 + x
+k3 = 2*x^3 + 2*x^2
+# k4_1 = 4*x^4 + 4*x^3
+# k4_2 = x^4 + 2*x^3 + 2*x^2 + x
 
-class main:
-  def __init__(self):
-    self.available_resistors = []
-    self.resultant_configs = []
-    
-  def exact_match(self):
-    if input in self.available_resistors:
-      return True
-  
-  def calc_comb(self):
-    
-    
-  def special_cases(self):
-    pass
+calculated_combinations += resistor_inventory
+x -= 1 # decrease by 1 to be used as the array index
+
+while x >= 0:
+    y = x
+    while y >= 0:
+        series = resistor_inventory[x] + resistor_inventory[y]
+        calculated_combinations.append(series)
+        parallel = (resistor_inventory[x] * resistor_inventory[y]) / series
+        calculated_combinations.append(parallel)
+        y -= 1
+    x -= 1
+
+print(calculated_combinations)
