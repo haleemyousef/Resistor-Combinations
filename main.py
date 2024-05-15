1e0d21fc929e26e97161ab979cb919367d5a116d
haleemyousef <abdulhalem.abusalem1@gmail.com>
2024-05-15 20:44:44 +0100

    fixed a bug due to incorrect symbol for power op

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-resistor_inventory = [1000, 2000]
+resistor_inventory = [1000, 2000, 5000, 10000, 50000]
 calculated_combinations = []
 
 n = len(resistor_inventory)
 k1 = n
-k2 = n^2 + n
-k3 = 2*n^3 + 2*n^2
+k2 = n**2 + n
+k3 = 2 * n**3 + 2 * n**2
 # k4_1 = 4*n^4 + 4*n^3
 # k4_2 = n^4 + 2*n^3 + 2*n^2 + n
 
@@ -27,3 +27,4 @@ compute_and_push(k1, k1, 0)
 compute_and_push(k1+k2, k1, k1, 0)
 
 print(calculated_combinations)
+print(len(calculated_combinations), k1+k2+k3)
